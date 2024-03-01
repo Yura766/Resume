743801746995ae9e8bcc0931ac2c3f83dd12abe9
Yura766 <workyura7@gmail.com>
2024-03-01 23:10:00 +0200

    try gltf on server

--- app/js/main.js
+++ app/js/main.js
@@ -17,23 +17,23 @@ sound.addEventListener('click', (e) => {
         soundSvg.children[2].style.opacity = 1;
     }
 })
-// // cursor
-// gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
 
-// let xTo = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
-//     yTo = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
+// cursor
+gsap.set(".cursor", { xPercent: -50, yPercent: -50 });
 
-// window.addEventListener("mousemove", e => {
-//     xTo(e.clientX);
-//     yTo(e.clientY);
-// });
+let xTo = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
+    yTo = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
 
-// window.addEventListener("mouseover", e => {
-//     // e.relatedTarget.localName == 'button'
-    
-//     // document.querySelector('.cursor').style.background = 'red'
-// })
+window.addEventListener("mousemove", e => {
+    xTo(e.clientX);
+    yTo(e.clientY);
+});
 
+window.addEventListener("mouseover", e => {
+    // e.relatedTarget.localName == 'button'
+
+    // document.querySelector('.cursor').style.background = 'red'
+})
 
 // nav pin
 ScrollTrigger.create({
@@ -52,6 +52,7 @@ const section = gsap.utils.toArray(".section")
 const navItem = gsap.utils.toArray(".nav__item")
 
 navItem.forEach((element, i) => {
+
     element.addEventListener('click', (e) => {
         gsap.to(window, { scrollTo: section[i] });
     })
@@ -61,7 +62,7 @@ navItem.forEach((element, i) => {
 section.forEach((e, i) => {
     ScrollTrigger.create({
         trigger: e,
-        markers: true,
+        // markers: true,
         start: "top center",
         end: "bottom center",
         onToggle: (self) => self.isActive == true ? navItem[i].classList.add("active") : navItem[i].classList.remove("active")
@@ -69,44 +70,4 @@ section.forEach((e, i) => {
 });
 
 
-// function setActive(e) {
-//     // navItem.forEach(el => el.classList.remove("active"));
-//     e.classList.add("active");
-// }
-
-
-//
-
-
-
-// ScrollTrigger.create({
-//     trigger: '.welcom__section',
-//     start: "top  top",
-//     end: '100% top',
-//     markers: true,
-//     onEnter: ({ progress, direction, isActive }) => {
-//         console.log('onEnter' + " - " + progress, direction, isActive)
-//         gsap.to(window, { duration: .5, scrollTo: ".about__section" });
-//     },
-//     onEnterBack: ({ progress, direction, isActive }) => {
-//         console.log('onEnterBack' + " - " + progress, direction, isActive)
-//         gsap.to(window, { duration: .5, scrollTo: ".welcom__section" });
-//     },
-
-
-//     // onLeave: ({ progress, direction, isActive }) => console.log('onLeave' + " - " + progress, direction, isActive),
-//     // onLeaveBack: ({ progress, direction, isActive }) => {
-//     //     console.log('onLeaveBack' + " - " + progress, direction, isActive)
-//     // },
-
-
-// })
-// section.forEach((e, i) => {
-
-
-// })
-
-
-
-// three js
-
+console.log('main.js +');
--- app/js/three.js
+++ app/js/three.js
@@ -1,9 +1,7 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-
 // приклад 1
-
 //сцена
 const scene = new THREE.Scene();
 
@@ -33,15 +31,15 @@ const aLight = new THREE.AmbientLight(0x404040, 1.2);
 scene.add(aLight);
 
 const pLight = new THREE.PointLight(0xFFFFFF, 1.2);
-pLight.position.set(0,-3,7);
+pLight.position.set(0, -3, 7);
 scene.add(pLight);
 
 
 // Добавляем некоторые базовые объекты на сцену
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// const geometry = new THREE.BoxGeometry();
+// const material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+// const cube = new THREE.Mesh(geometry, material);
+// scene.add(cube);
 
 const sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
 const sphereMaterial = new THREE.MeshPhongMaterial({ color: 0xff0000 });
@@ -50,28 +48,38 @@ sphere.position.x = -3;
 scene.add(sphere);
 
 
-const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
-const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
+// const planeGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+// const planeMaterial = new THREE.MeshPhongMaterial({ color: 0x0000ff, side: THREE.DoubleSide });
+// const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+// plane.rotation.x = -Math.PI / 2;
+// scene.add(plane);
 
 // Добавляем управление орбитой для перемещения камеры
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 //our 3d-obj
 let loader = new GLTFLoader();
 
-loader.load('/models/table.gltf',function(gltf){
+
+function getModel(url) {
+    console.log(url);
+    loader.load(url, function (gltf) {
         const mesh = gltf.scene;
 
-    scene.add(mesh);
-}); 
+        scene.add(mesh);
+    });
+}
+
+getModel('/untitled.gltf')
+
+
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
     renderer.render(scene, camera);
 }
 animate();
+
+console.log('three.js +');
